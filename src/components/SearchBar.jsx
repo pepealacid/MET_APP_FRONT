78f8ce776ebb193f5fbf77b7d668168f677b645f
pepealacid <ppalacid@gmail.com>
2023-07-05 12:50:12 +0200

    searchbar working but sometimes too heavy

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,58 +1,213 @@
-import { useState } from "react";
-import {
-  Input,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  PopoverHeader,
-  PopoverBody,
-  VStack,
-} from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { Input, Box, Image, Text, Spinner, Checkbox } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 export default function SearchBar() {
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [isHighlightChecked, setIsHighlightChecked] = useState(false);
+  const [typingTimeout, setTypingTimeout] = useState(null);
 
-  const handleInputChange = async (event) => {
+  useEffect(() => {
+    if (searchQuery.trim() !== "") {
+      const fetchSearchResults = async () => {
+        setLoading(true);
+
+        try {
+          let apiUrl = `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${searchQuery}`;
+
+          if (isHighlightChecked) {
+            apiUrl += "&isHighlight=true";
+          }
+
+          const response = await axios.get(apiUrl);
+          const objectIDs = response.data.objectIDs;
+          const promises = objectIDs.map((objectID) =>
+            axios.get(
+              `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`
+            )
+          );
+          const objectResponses = await Promise.all(promises);
+          const objects = objectResponses.map(
+            (objectResponse) => objectResponse.data
+          );
+          setSearchResults(objects);
+        } catch (error) {
+          console.error("Error fetching search results:", error);
+        }
+
+        setLoading(false);
+      };
+
+      // Clear previous typing timeout
+      if (typingTimeout) {
+        clearTimeout(typingTimeout);
+      }
+
+      // Set new typing timeout
+      const newTypingTimeout = setTimeout(() => {
+        fetchSearchResults();
+      }, 2000);
+
+      setTypingTimeout(newTypingTimeout);
+    } else {
+      setSearchResults([]);
+    }
+  }, [searchQuery, isHighlightChecked]);
+
+  const handleInputChange = (event) => {
     const query = event.target.value;
     setSearchQuery(query);
+  };
 
-    try {
-      const response = await axios.get(
-        `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${query}`
-      );
-      setSearchResults(response.data.objectIDs);
-      console.log(query, searchResults)
-    } catch (error) {
-      console.error("Error fetching search results:", error);
-    }
+  const handleHighlightChange = (event) => {
+    const checked = event.target.checked;
+    setIsHighlightChecked(checked);
+  };
+
+  const filterByArtist = (results) => {
+    const artistResults = results.filter((result) =>
+      result.artistDisplayName.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    return artistResults.slice(0, 3);
+  };
+
+  const filterByArtworkTitle = (results) => {
+    const artworkResults = results.filter((result) =>
+      result.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    return artworkResults.slice(0, 3);
+  };
+
+  const filterByOthers = (results) => {
+    const otherResults = results.filter(
+      (result) =>
+        !result.artistDisplayName
+          .toLowerCase()
+          .includes(searchQuery.toLowerCase()) &&
+        !result.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    return otherResults.slice(0, 3);
   };
 
   return (
-    <Popover>
-      <PopoverTrigger>
-        <Input
-          placeholder="Busca un artista u obra"
-          size="md"
-          value={searchQuery}
-          onChange={handleInputChange}
-        />
-      </PopoverTrigger>
-      <PopoverContent>
-        <PopoverHeader>Search Results</PopoverHeader>
-        <PopoverBody>
-          <VStack spacing={2}>
-            {searchResults.length > 0 ? (
-              searchResults.map((result) => (
-                <div key={result.objectID}>{result.title}</div>
-              ))
-            ) : (
-              <div>No results found</div>
-            )}
-          </VStack>
-        </PopoverBody>
-      </PopoverContent>
-    </Popover>
+    <Box position="relative">
+      <Input
+        placeholder="Search for an artist or artwork"
+        size="md"
+        value={searchQuery}
+        onChange={handleInputChange}
+      />
+      <Checkbox mt={2} onChange={handleHighlightChange}>
+        Show only highlights
+      </Checkbox>
+      {loading && (
+        <Box
+          position="absolute"
+          zIndex={10}
+          top="100%"
+          left={0}
+          right={0}
+          backgroundColor="white"
+          boxShadow="md"
+          borderRadius="md"
+          p={4}
+          mt={2}
+          maxHeight="300px"
+          overflowY="auto"
+        >
+          <Spinner size="sm" color="gray.500" mr={2} />
+        </Box>
+      )}
+      {searchQuery.trim() !== "" && !loading && searchResults.length > 0 && (
+        <Box
+          position="absolute"
+          zIndex={10}
+          top="100%"
+          left={0}
+          right={0}
+          backgroundColor="white"
+          boxShadow="md"
+          borderRadius="md"
+          p={4}
+          mt={2}
+          maxHeight="300px"
+          overflowY="auto"
+        >
+          {filterByArtist(searchResults).length > 0 && (
+            <>
+            <Text fontWeight="bold">Works from "{searchQuery.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}"</Text>
+
+              {filterByArtist(searchResults).map((result) => (
+                <Link
+                  key={result.objectID}
+                  to={`/details/${result.objectID}`}
+                  style={{ textDecoration: "none" }}
+                >
+                  <Box display="flex" alignItems="center" mb={2}>
+                    <Image
+                      src={result.primaryImageSmall}
+                      alt={result.title}
+                      boxSize="60px"
+                      objectFit="cover"
+                      marginRight={4}
+                    />
+                    <Text>{result.title}</Text>
+                  </Box>
+                </Link>
+              ))}
+            </>
+          )}
+          {filterByArtworkTitle(searchResults).length > 0 && (
+            <>
+              <Text fontWeight="bold">Artworks called "{searchQuery.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}":</Text>
+              {filterByArtworkTitle(searchResults).map((result) => (
+                <Link
+                  key={result.objectID}
+                  to={`/details/${result.objectID}`}
+                  style={{ textDecoration: "none" }}
+                >
+                  <Box display="flex" alignItems="center" mb={2}>
+                    <Image
+                      src={result.primaryImageSmall}
+                      alt={result.title}
+                      boxSize="60px"
+                      objectFit="cover"
+                      marginRight={4}
+                    />
+                    <Text>{result.title}</Text>
+                  </Box>
+                </Link>
+              ))}
+            </>
+          )}
+          {filterByOthers(searchResults).length > 0 && (
+            <>
+              <Text fontWeight="bold">Other results:</Text>
+              {filterByOthers(searchResults).map((result) => (
+                <Link
+                  key={result.objectID}
+                  to={`/details/${result.objectID}`}
+                  style={{ textDecoration: "none" }}
+                >
+                  <Box display="flex" alignItems="center" mb={2}>
+                    <Image
+                      src={result.primaryImageSmall}
+                      alt={result.title}
+                      boxSize="60px"
+                      objectFit="cover"
+                      marginRight={4}
+                    />
+                    <Text>{result.title}</Text>
+                  </Box>
+                </Link>
+              ))}
+            </>
+          )}
+        </Box>
+      )}
+    </Box>
   );
 }
