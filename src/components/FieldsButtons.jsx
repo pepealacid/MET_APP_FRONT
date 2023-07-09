c9c670a3b40ee51412c7fd30dbb2c2ddf853f32a
pepealacid <ppalacid@gmail.com>
2023-07-10 00:25:51 +0200

    Field buttons added to artists search page

--- src/components/FieldsButtons.jsx
+++ src/components/FieldsButtons.jsx
@@ -1,8 +1,8 @@
 import { Button } from "@chakra-ui/react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function FieldsButtons() {
-  const { field } = useParams();
+  const location = useLocation();
   const navigate = useNavigate();
 
   const handleClick = (fieldValue) => {
@@ -12,21 +12,21 @@ function FieldsButtons() {
   return (
     <div>
       <Button
-        colorScheme={field === "tours" ? "blue" : undefined}
+        colorScheme={location.pathname === "/home/tours" ? "blue" : undefined}
         onClick={() => handleClick("tours")}
       >
         Tours
       </Button>
 
       <Button
-        colorScheme={field === "artists" ? "blue" : undefined}
+        colorScheme={location.pathname === "/home/artists" ? "blue" : undefined}
         onClick={() => handleClick("artists")}
       >
         Artists
       </Button>
 
       <Button
-        colorScheme={field === "artworks" ? "blue" : undefined}
+        colorScheme={location.pathname === "/home/artworks" ? "blue" : undefined}
         onClick={() => handleClick("artworks")}
       >
         Artworks
--- src/pages/ArtistSearchPage.jsx
+++ src/pages/ArtistSearchPage.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Grid, GridItem } from "@chakra-ui/react";
 import ArtistCard from "../components/ArtistCard/ArtistCard";
 import { Link } from "react-router-dom";
+import FieldsButtons from "../components/FieldsButtons";
 
 const ArtistSearchPage = () => {
   const [results, setResults] = useState([]);
@@ -16,13 +17,17 @@ const ArtistSearchPage = () => {
   return (
     <div>
       <ArtistsSearchBar updateResults={updateResults} />
+      <FieldsButtons />
       {results.length > 0 && (
         <Grid templateColumns="repeat(2, 1fr)" gap={4}>
           {results.map((result) => (
             result.id !== null && (
               <GridItem key={result.id}>
                 <Link
-                  to={`/artist/${result.id}`}
+                  to={{
+                  pathname: `/artist/${result.title}`,
+                  search: `?url=${encodeURIComponent(result._links.self.href)}`,
+                }}
                   style={{ textDecoration: "none", cursor: "pointer" }}
                 >
                   <ArtistCard
