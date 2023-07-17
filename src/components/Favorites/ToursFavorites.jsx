719f1b4652b3f8d7d4d789f1ab25e0a73c2d5cdf
AngelesVFA <spotifyasuna21@gmail.com>
2023-07-17 21:15:41 +0200

    mandatory pull

--- src/components/Favorites/ToursFavorites.jsx
+++ src/components/Favorites/ToursFavorites.jsx
@@ -1,10 +1,42 @@
+import { useState, useContext, useEffect } from "react";
 import FavFieldsButtons from "../FavFieldsButtons";
+import NoElementsFound from "./NoElementsFound";
+import itineraryService from "../../services/itinerary.service";
+import { AuthContext } from "../../context/auth.context";
+import authService from "../../services/auth.service";
+
 
 const ToursFavorites = () => {
+  // const [savedTour, setSaveTour] = useState(null)
+  // const { user } = useContext(AuthContext)
+  
+  // useEffect(()=>{
+  //   console.log(user)
+
+  // }, [])
+  
+  
+  // const getUserSavedItineraries = async()=>{
+  //   const userId = getUserId()
+  //   const saved = await itineraryService.getUserItineraries(userId)
+  //   return saved
+  //   console.log(saved.data)
+  // }
+  // const getUserId = () => {
+  //   return user
+  // }
+
+  // getUserSavedItineraries()
+
+
   return (
     <div>
       <h3>ToursFavorites</h3>
       <FavFieldsButtons />
+      {savedTour? 
+      <>Poner aquí los tours de api</>:
+      <NoElementsFound> tours </NoElementsFound>
+    }
     </div>
   );
 };
--- src/services/itinerary.service.js
+++ src/services/itinerary.service.js
@@ -10,6 +10,9 @@ class ItineraryService {
     addItinerary(userId, data){
         return this.api.put(`/user/itineraries/${userId}`, data)
     }
+    getUserItineraries(userId){
+        return this.api.get(`/user/itineraries/${userId}`)
+    }
 }
 
 const itineraryService = new ItineraryService();
