0d6bce8e92ea8b3efea850cc1ab61ab21ec62053
pepealacid <ppalacid@gmail.com>
2023-07-22 11:38:25 +0200

    Emergency SOS

--- src/components/Itinerary/fav/FavCard.jsx
+++ src/components/Itinerary/fav/FavCard.jsx
@@ -1,42 +1,31 @@
-import { Grid, Heading, Image } from "@chakra-ui/react"
+import { Grid, Heading, Image } from "@chakra-ui/react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import "./FavCard.css";
 
-
-
-
-
 function FavCard({ tour }) {
+  const tourId = tour._id;
+  const navigate = useNavigate();
 
-    const [hasBeenClick, setHasBeenClick] = useState(false)
-
-
-    const tourId = tour._id
-    const navigate = useNavigate()
-
-    const goToDetailsPage = () => {
-        navigate(`/itinerary/${tourId}`)
-    }
-    return (
-        <>
-                {hasBeenClick && (
-                    <Navigate to={url} />
-                )}
-                <div className="little-artwork-card" onClick={goToDetailsPage}>
-                    <div className="top">
-                        <div className="img-container">
-                            <img className="main-img" src={tour.picture} alt="artwork" />
-                        </div>
-                    </div>
-                    <div className="foot">
-                    <Heading as='h6' size='xs'>{tour.name}</Heading>
-
-                    </div>
-                    
-                </div>
-        </>
-    )
+  const goToDetailsPage = () => {
+    navigate(`/itinerary/${tourId}`);
+  };
+  return (
+    <>
+      <div className="little-artwork-card" onClick={goToDetailsPage}>
+        <div className="top">
+          <div className="img-container">
+            <img className="main-img" src={tour.picture} alt="artwork" />
+          </div>
+        </div>
+        <div className="foot">
+          <Heading as="h6" size="xs">
+            {tour.name}
+          </Heading>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default FavCard
+export default FavCard;
